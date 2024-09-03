a22945a1ddf73b05d524e29ba6efa5949857ed3c
Johannes <johannes.widkopp@gmail.com>
2024-09-03 08:34:50 +0300

    uusi yritys

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('')
+  const [heartratelow, setHeartRateLow] = useState(0)
+  const [heartratehigh, setHeartRateHigh] = useState(0)
+
+  useEffect(() => {
+    if (age) {
+        setHeartRateLow((220 - age) * 0.65);
+        setHeartRateHigh((220 - age) * 0.85); 
+    } else {
+      setHeartRateLow(0);
+      setHeartRateHigh(0);
+    }
+  }, [age]);
+  
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput 
+        placeholder='enter age'
+        value={age}
+        onChangeText={text => setAge(text)}
+        keyboardType='decimal-pad'
+      />
+      <Text>Limits</Text>
+      <Text>{heartratelow} - {heartratehigh}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +38,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-start',
+    marginTop: 50,
+    marginLeft: 10,
   },
 });
